5cafad814b4fe01b1c04b8787c05c0d9b560c687
FinalCore <rimedheart@yandex.ru>
2020-01-16 22:50:58 +0300

    Заполнил класс Element в проекте 019_Custom Collection

---
+++ 019_Project/Element.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _019_Project
+{
+    class Element : IComparable
+    {
+        string name;
+        int number;
+
+        public string Name { get; set; } 
+        public int Number { get; set; } 
+
+        public Element (string name, int number)
+        {
+            this.name = name;
+            this.number = number;
+        }
+
+        public int CompareTo(object obj)
+        {
+
+            if (this.number > ((Element)obj).number)
+                return 1;
+            else if (this.number == ((Element)obj).number)
+                return 0;
+            else return -1;
+        }
+    }
+}
