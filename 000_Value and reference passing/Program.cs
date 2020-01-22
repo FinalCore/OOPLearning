3e4a76d764bb98c9105cae9627aef69d30c7f747
FinalCore <rimedheart@yandex.ru>
2020-01-22 08:31:03 +0300

    Добавил проекты 000_ConstructorChain и 000_Value and reference passing

---
+++ 000_Value and reference passing/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _000_Value_and_reference_passing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Person p = new Person("Fred", 12);
+            p.Display();
+            //Передача полей класса (ссылочного типа) по значению
+            // SendAPersonByValue(p);
+            SendAPersonByReference(ref p);
+            p.Display();
+            Console.ReadKey();
+        }
+
+        //Метод для передачи полей класса (ссылочного типа) по значению
+        static void SendAPersonByValue(Person p)
+        {
+            p.age = 99;
+            p = new Person("Nikki", 99);
+        }
+
+        static void SendAPersonByReference(ref Person p)
+        {
+            p.age = 999;
+            p = new Person("Nikki", 999);
+        }
+    }
+
+    class Person
+    {
+        public string name;
+        public int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public Person() { }
+
+        public void Display()
+        {
+            Console.WriteLine($"{this.name} {this.age}");
+        }
+    }
+}
