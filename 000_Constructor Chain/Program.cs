911b6c27b2281e24847d0768e94a5f846d14cf34
FinalCore <rimedheart@yandex.ru>
2020-04-10 12:55:42 +0300

    Начал делать примеры с многопоточностью

---
+++ 000_Constructor Chain/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _000_Constructor_Chain
+{
+    #region Цепочка конструкторов на примере базового класса
+
+    public class Animal
+    {
+        string type;
+        int age;
+        int weight;
+        bool isMale;
+
+        public Animal() { }
+
+        public Animal(string t)
+        {
+            type = t;
+        }
+
+        public Animal(string t, int a) : this(t)
+        {
+            age = a;
+        }
+             
+        public Animal (string t, int a, int w, bool isMale) : this (t, a)
+        {
+            weight = w;
+            this.isMale = isMale;
+        }
+    }
+
+
+
+    #endregion
+
+    #region Цепочка конструкторов и наследование
+    public class Person
+    {
+        int Id;
+        string name;
+
+        public Person(int id, string name)
+        {
+            this.Id = id;
+            this.name = name;
+        }
+    }
+
+    public class Employee : Person 
+    {
+        string position;
+        public Employee (string position, int id, string name) : base (id, name)
+        {
+            this.position = position;
+        }
+    }
+
+    #endregion
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal c = new Animal("cat");
+            Animal b = new Animal("lion", 5, 150, true);
+
+            Person p = new Person(1, "Vasya");
+            Employee e = new Employee("manager", 1, "Petya");
+
+        }
+    }
+}
---
+++ 005.01_Delegates/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _005._01_Delegates
+{
+    public class Car
+    {
+        public int CurrSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        private bool CarIsDead { get; set; }
+
+        public Car(int maxSpeed, int currSpeed)
+        {
+            CurrSpeed = currSpeed;
+            MaxSpeed = maxSpeed;
+        }
+
+        public delegate void CarEngineHandler(string msgForCaller);
+        public CarEngineHandler listOfHandlers;
+
+        //public void Accelerate(int delta)
+        //{
+        //    //if (CarIsDead)
+        //        //listOfHandlers(msg1);
+        //    else CurrSpeed += delta;
+        //    if (MaxSpeed - CurrSpeed <= 10)            
+        //        listOfHandlers("Be careful, the engine is hot");
+        //    if(CurrSpeed > MaxSpeed)
+        //    {
+        //        listOfHandlers("Ooops, it seems smbd go home by foot");
+        //        CarIsDead = true;
+        //    }
+        //    else Console.WriteLine($"Current speed = {CurrSpeed}");                
+        //}           
+
+    }
+    public class Program
+    {
+        public string msg1 = "Sorry, this car is dead";
+        static void Main(string[] args)
+        {
+            Car car = new Car(120, 20);
+        
+            void OnCarEngineEvent()
+            {
+
+            }
+        }
+    }
+}
